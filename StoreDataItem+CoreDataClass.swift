e4a6926da7ce8aa9a4b12888045fbc70f1b8dbf5
Rajnish Sharma <rajnish.sharma1411@gmail.com>
2023-02-18 23:55:37 +0530

    UPDATE - Converted StoreCoreDataModel codegen to Manual

---
+++ StoreDataItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StoreDataItem+CoreDataClass.swift
+//  store-app
+//
+//  Created by Rajnish Sharma on 18/02/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StoreDataItem)
+public class StoreDataItem: NSManagedObject {
+
+}
