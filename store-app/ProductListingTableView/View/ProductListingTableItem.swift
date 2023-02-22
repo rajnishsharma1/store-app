8050e1aea0513d741d22458272fbc2305dc483cf
Rajnish Sharma <rajnish.sharma1411@gmail.com>
2023-02-22 22:25:34 +0530

    UPDATE - VFL to NSLayoutContraints

--- store-app/ProductListingTableView/View/ProductListingTableItem.swift
+++ store-app/ProductListingTableView/View/ProductListingTableItem.swift
@@ -69,33 +69,86 @@ class ProductListingTableItem : UITableViewCell {
         contentView.addSubview(extra)
         contentView.addSubview(mrpPlaceHolder)
         contentView.addSubview(divider)
+        
+        // Adding constraints for UI Elements
+        addImageConstraints()
+        addItemNameContstraints()
+        addItemPriceConstraints()
+        addMrpPlaceholderConstraints()
+        addExtraItemDetailsConstraints()
+        addDividerConstraint()
+    }
+    
+    // MARK: - Constraints for Item Image
+    /// Constraints for ItemImage
+    private func addImageConstraints() {
+        let imageTop = NSLayoutConstraint(item: itemImage, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1, constant: 20)
+        
+        let imageLeading = NSLayoutConstraint(item: itemImage, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1, constant: 34)
+        
+        let imageHeight = NSLayoutConstraint(item: itemImage, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+        
+        let imageWidth = NSLayoutConstraint(item: itemImage, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+        
+        contentView.addConstraints([imageTop, imageLeading, imageHeight, imageWidth])
+    }
+    
+    // MARK: - Constraints for Item Name
+    /// Constraints for ItemName
+    private func addItemNameContstraints() {
+        
+        let itemNameLeading = NSLayoutConstraint(item: itemName, attribute: .leading, relatedBy: .equal, toItem: itemImage, attribute: .trailing, multiplier: 1, constant: 16)
+        
+        let itemNameTop = NSLayoutConstraint(item: itemName, attribute: .top, relatedBy: .equal, toItem: itemImage, attribute: .top, multiplier: 1, constant: 0)
+        
+        contentView.addConstraints([itemNameLeading, itemNameTop])
+    }
+    
+    // MARK: - Constraints for MRP Placeholder
+    /// Constraints for MRP Placeholder
+    private func addMrpPlaceholderConstraints() {
+        let mrpPlaceholderTop = NSLayoutConstraint(item: mrpPlaceHolder, attribute: .top, relatedBy: .equal, toItem: itemName, attribute: .bottom, multiplier: 1, constant: 8)
+        
+        let mrpPlaceholderLeading = NSLayoutConstraint(item: mrpPlaceHolder, attribute: .leading, relatedBy: .equal, toItem: itemImage, attribute: .trailing, multiplier: 1, constant: 16)
+        
+        contentView.addConstraints([mrpPlaceholderTop, mrpPlaceholderLeading])
+    }
+    
+    // MARK: - Constraints for Item Price
+    /// Constraints for ItemPrice
+    private func addItemPriceConstraints() {
+        let itemPriceTop = NSLayoutConstraint(item: itemPrice, attribute: .top, relatedBy: .equal, toItem: mrpPlaceHolder, attribute: .top, multiplier: 1, constant: 0)
+        
+        let itemPriceLeading = NSLayoutConstraint(item: itemPrice, attribute: .leading, relatedBy: .equal, toItem: mrpPlaceHolder, attribute: .trailing, multiplier: 1, constant: 0)
+        
+        contentView.addConstraints([itemPriceTop, itemPriceLeading])
+    }
+    
+    // MARK: - Constraints for Item Details
+    /// Constraints for ItemDetails
+    private func addExtraItemDetailsConstraints() {
+        let extraTrailing = NSLayoutConstraint(item: extra, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1, constant: -42)
+        
+        let extraTop = NSLayoutConstraint(item: extra, attribute: .top, relatedBy: .equal, toItem: itemPrice, attribute: .top, multiplier: 1, constant: 0)
+        
+        let extraBottom = NSLayoutConstraint(item: extra, attribute: .bottom, relatedBy: .equal, toItem: itemPrice, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        contentView.addConstraints([extraTrailing, extraBottom, extraTop])
+    }
+    
+    // MARK: - Constraints for Divider
+    /// Constraints for Divider
+    private func addDividerConstraint() {
+        let dividerLeading = NSLayoutConstraint(item: divider, attribute: .leading, relatedBy: .equal, toItem: itemImage, attribute: .trailing, multiplier: 1, constant: 16)
+        
+        let dividerTop = NSLayoutConstraint(item: divider, attribute: .top, relatedBy: .equal, toItem: mrpPlaceHolder, attribute: .bottom, multiplier: 1, constant: 10)
+        
+        let dividerTrailing = NSLayoutConstraint(item: divider, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1, constant: -34)
+        
+        let dividerBottom = NSLayoutConstraint(item: divider, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0)
 
-        // Mappding views to string to set the constraints
-        let viewsDict = [
-            "storeImage" : itemImage,
-            "itemName" : itemName,
-            "itemPrice" : itemPrice,
-            "extra" : extra,
-            "mrpPlaceHolder": mrpPlaceHolder,
-            "divider": divider
-        ] as [String : Any]
-
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[storeImage(50)]", options: [], metrics: nil, views: viewsDict))
+        let dividerHeight = NSLayoutConstraint(item: divider, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 1)
         
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-40-[extra]", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[itemName]-[itemPrice]", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[mrpPlaceHolder]-10-[divider(0.5)]", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[itemPrice]-10-[divider(0.5)]", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[itemName]-[mrpPlaceHolder]-|", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[storeImage(50)]-[itemName]|", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[storeImage(50)]-[mrpPlaceHolder]-[itemPrice]-[extra]-|", options: [], metrics: nil, views: viewsDict))
-        
-        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[storeImage(50)]-[divider]-16-|", options: [], metrics: nil, views: viewsDict))
+        contentView.addConstraints([dividerLeading, dividerTop, dividerTrailing, dividerHeight, dividerBottom])
     }
 }
